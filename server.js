32f60554d132e480225203743b2e36cc3acecb19
swiftFox <rcwilson995@gmail.com>
2016-08-04 18:11:42 +0100

    Refactor middlewear

--- server.js
+++ server.js
@@ -2,16 +2,7 @@ var express = require('express');
 var app = express();
 const PORT = 3000;
 
-var middlewear = {
-    requireAuthentication: function (req, res, next) {
-        console.log('Private route hit');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middlewear = require('./middlewear.js');
 
 app.use(middlewear.logger);
 
